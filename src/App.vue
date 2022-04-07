d01ac07b2b48b637ae6745980f37a2d28846db96
bremxx <petr.kirienko@gmail.com>
2022-04-07 20:16:48 +0300

    task-card component added

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
   <div>
-    <div v-if="!isDataLoaded">Загрузка...</div>
     <input v-model="newTaskText" ref="input" placeholder="Новая задача" />
     <button @click="addTask">Добавить задачу</button>
 
-    <div>
-      <button @click="setFilter('active')">Активные</button>
-      <button @click="setFilter('all')">Все</button>
-      <button @click="setFilter('completed')">Завершенные</button>
+    <div class="filters">
+      <button @click="setFilter(Filter.ACTIVE)" class="filter-item">Активные</button>
+      <button @click="setFilter(Filter.ALL)" class="filter-item">Все</button>
+      <button @click="setFilter(Filter.COMPLETED)" class="filter-item">Завершенные</button>
     </div>
 
-    <ul>
-      <li v-for="({ text, id }, i) in visibleTasks" :key="`task-${id}`">
-        {{ text }}
-        <button @click="setCompleted(i)">Сделано!</button>
+    <ul v-if="isDataLoaded" class="task-list">
+      <li v-for="{ text, id } in visibleTasks" :key="`task-${id}`">
+        <task-card :text="text" @setTaskCompleted="setCompleted(id)" />
       </li>
     </ul>
+    <p v-else>Загрузка...</p>
   </div>
 </template>
 
 <script>
+import TaskCard from "./components/task-card.vue";
+
 export default {
   name: "App",
+  components: { TaskCard },
   data() {
     return {
       tasks: [],
@@ -29,6 +31,11 @@ export default {
       activeFilter: "all",
       newTaskText: "",
       isDataLoaded: false,
+      Filter: {
+        ALL: "all",
+        ACTIVE: "active",
+        COMPLETED: "completed",
+      },
     };
   },
   created() {
@@ -39,12 +46,21 @@ export default {
   },
   methods: {
     addTask() {
-      this.tasks.push({ text: this.newTaskText });
+      this.tasks.push({
+        text: this.newTaskText,
+        id: this.tasks.length + 1,
+        active: true,
+      });
       this.newTaskText = "";
     },
-    setCompleted(i) {
-      const task = this.tasks[i];
-      this.$set(this.tasks, i, { ...task, active: true });
+    setCompleted(id) {
+      const index = this.tasks.findIndex((t) => t.id === id);
+      const task = this.tasks[999];
+      if (!task) {
+        console.error(`Задача с индексом: ${index} не найдена!`);
+        return;
+      }
+      this.$set(this.tasks, index, { ...task, active: false });
     },
     setFilter(filter) {
       this.activeFilter = filter;
@@ -53,9 +69,9 @@ export default {
     filterTasks() {
       this.visibleTasks = this.tasks.filter((task) => {
         switch (this.activeFilter) {
-          case "completed":
+          case this.Filter.COMPLETED:
             return !task.active;
-          case "active":
+          case this.Filter.ACTIVE:
             return task.active;
           default:
             return true;
@@ -72,21 +88,17 @@ export default {
           this.tasks = data;
           this.isDataLoaded = true;
         } else {
-          console.error(`Ошибка, статус ответа ${res.status}`)
+          console.error(`Ошибка запроса, статус ответа ${res.status}`);
         }
-      } catch (error) {
-        console.error(error.message)
+      } catch (e) {
+        throw new Error(`Ошибка fetch: ${e.message}`)
       }
     },
   },
   watch: {
-    isDataLoaded() {
+    tasks() {
       this.filterTasks();
-    }
-  }
+    },
+  },
 };
 </script>
-
-<style scoped>
-
-</style>
---
+++ src/components/task-card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="task">
+    <span class="task-text">{{ text }}</span>
+    <button @click="setTaskCompleted">Сделано!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskList",
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: ``,
+    },
+  },
+  methods: {
+    setTaskCompleted() {
+      this.$emit(`setTaskCompleted`)
+    },
+  },
+};
+</script>
