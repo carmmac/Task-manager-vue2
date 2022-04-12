32e94cb50aeeb379d9f7e1538a29b6a6c3d614b3
bremxx <petr.kirienko@gmail.com>
2022-04-12 15:56:41 +0300

    edited task-card appearence

--- src/components/task-card.vue
+++ src/components/task-card.vue
@@ -1,19 +1,16 @@
 <template>
-  <div>
-    <label :for="`check-${id}`" class="me-2 pointer">
+  <div class="d-flex align-items-center">
+    <label :for="`check-${id}`" class="me-2">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="16"
         height="16"
-        fill="currentColor"
-        :class="`bi bi-check-square ${iconStyle}`"
+        :fill="currentColor"
+        class="bi bi-check-square-fill"
         viewBox="0 0 16 16"
       >
         <path
-          d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
-        />
-        <path
-          d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"
+          d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"
         />
       </svg>
     </label>
@@ -48,8 +45,8 @@ export default {
     },
   },
   computed: {
-    iconStyle() {
-      return this.active ? "active" : "";
+    currentColor() {
+      return this.active ? "#7de29f" : "white";
     },
   },
   methods: {
@@ -66,13 +63,5 @@ export default {
   opacity: 0;
   height: 0;
   width: 0;
-}
-
-.pointer {
-  cursor: pointer;
-}
-
-.active {
-  background-color: #7de29f;
 }
 </style>
